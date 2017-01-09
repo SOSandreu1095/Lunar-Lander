6142a156faae56dd41456d24c874490d3c105014
SOSandreu1095 <sos.andreu.sos@gmail.com>
2017-01-10 00:55:15 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -7,9 +7,17 @@ var timer=null;
 var timerFuel=null;
 var fuel=100;
 
+var paused=false;
+var end=false;
+var maxY = 70; //Trayectoria maxima en Y
+var mode="medium";
+
+
 //al cargar por completo la página...
 window.onload = function(){
-	//definición de eventos
+	//definición de Eventos
+	//el color del modo defecto
+	document.getElementById("medium").style.background = "#E30B32";
 	//mostrar menú móvil
     	document.getElementById("showm").onclick = function () {
 		document.getElementsByClassName("c")[0].style.display = "block";
@@ -20,6 +28,7 @@ window.onload = function(){
 		document.getElementsByClassName("c")[0].style.display = "none";
 		start();
 	}
+	
 	//encender/apagar el motor al hacer click en la pantalla
 	document.onclick = function () {
  	  if (a==g){
@@ -31,6 +40,51 @@ window.onload = function(){
 	//encender/apagar al apretar/soltar una tecla
 	document.onkeydown = motorOn;
 	document.onkeyup = motorOff;
+
+	//Eventos de las opciones
+	document.getElementById("pause").onclick = function (){
+		if (paused){
+			start();
+		} else {
+			stop();
+		}
+	}
+
+	document.getElementById("restart").onclick = function (){
+		restart();
+	}
+
+	document.getElementById("help").onclick = function (){
+		stop();
+		visualizarInstrucciones();
+	}
+
+	//ListHeadActions
+	document.getElementById("return").onclick = function (){
+		start();
+		ocultarInstrucciones();
+	}
+
+	document.getElementById("easy").onclick = function (){
+		mode = "easy";
+		document.getElementById("easy").style.background = "#E30B32";
+		document.getElementById("medium").style.background = "#810909";
+		document.getElementById("hard").style.background = "#810909";
+	}
+
+	document.getElementById("medium").onclick = function (){
+		mode = "medium";
+		document.getElementById("medium").style.background = "#E30B32";
+		document.getElementById("easy").style.background = "#810909";
+		document.getElementById("hard").style.background = "#810909";
+	}
+
+	document.getElementById("hard").onclick = function (){
+		mode = "hard";
+		document.getElementById("hard").style.background = "#E30B32";
+		document.getElementById("easy").style.background = "#810909";
+		document.getElementById("medium").style.background = "#810909";
+	}
 	
 	//Empezar a mover nave
 	start();
@@ -39,39 +93,79 @@ window.onload = function(){
 //Definición de funciones
 function start(){
 	timer=setInterval(function(){ moverNave(); }, dt*1000);
+	paused = false;
 }
-
+ 
 function stop(){
 	clearInterval(timer);
+	paused = true;
 }
 
 function moverNave(){
 	v +=a*dt;
 	document.getElementById("velocidad").innerHTML=v.toFixed(2);
 	y +=v*dt;
-	document.getElementById("altura").innerHTML=y.toFixed(2);
+	document.getElementById("altura").innerHTML=(maxY - y).toFixed(2);
 	
-	//mover hasta que top sea un 70% de la pantalla
-	if (y<70){ 
+	//mover hasta que top sea un 70% de la pantalla (maxY)
+	if ((y<maxY) && (y > 0)) { //Mientras no toque el techo ni suelo
 		document.getElementById("nave").style.top = y+"%"; 
-	} else { 
+	} else {
 		stop();
+		end = true;
+		checkColision();
 	}
 }
 function motorOn(){
 	a=-g;
-	if (timerFuel==null)
-	timerFuel=setInterval(function(){ actualizarFuel(); }, 10);	
-	//document.getElementById("naveImg").src = "../img/rocketOn.png";
+	if ((timerFuel==null) && (!end)){
+		timerFuel=setInterval(function(){ actualizarFuel(); }, 10);	
+		document.getElementById("naveImg").src = "img/rocketOn.png";
+	}
 }
 function motorOff(){
 	a=g;
-	clearInterval(timerFuel);
-	timerFuel=null;
-	//document.getElementById("naveImg").src = "../img/rocketOff.png";
+	if (!end){
+		clearInterval(timerFuel);
+		timerFuel=null;
+		document.getElementById("naveImg").src = "img/rocketOff.png";
+	}
 }
 function actualizarFuel(){
 	//Aquí hay que cambiar el valor del marcador de Fuel...
 	fuel-=0.1;
 	document.getElementById("fuel").innerHTML=fuel.toFixed(2);	
+}
+
+function restart(){
+	alert("Restart game not controled yet");
+}
+
+function visualizarInstrucciones(){
+	document.getElementById("helpDiv").style.display='block';
+}
+
+function ocultarInstrucciones(){
+	document.getElementById("helpDiv").style.display='none';
+}
+
+function checkColision(){
+	if (y < 0){ //Techo
+		document.getElementById("naveImg").src="img/firegif.gif";
+	} else { //Suelo
+		//1m/s en modo difícil, 5m/s en modo muy fácil 
+		switch(mode) {
+			case "easy": maxV = 5;
+				break;
+			case "medium": maxV = 3;
+				break;
+			case "hard": maxV = 1;
+			
+		}
+		if (v > maxV) {
+			document.getElementById("naveImg").src="img/firegif.gif"; 
+		} else {
+			alert("Congratulations");
+		}
+	}
 }
